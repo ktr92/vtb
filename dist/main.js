92ab88e5cdfc8d30670a77cd381d0203a4b8fe0d
Timur Kudashev <kudashev@ipotech.su>
2022-12-23 20:15:05 +0300

    initial

--- dist/main.js
+++ dist/main.js
@@ -1,23 +1,24 @@
 $(document).ready(function() {
-/*   document.querySelectorAll('.dropdown').forEach(dropdown => {
+  document.querySelectorAll('.dropdown').forEach(dropdown => {
     let $dropdown = dropdown
     $dropdown.addEventListener('click', event => {
-      $dropdown.querySelectorAll('.item').forEach(el=> {
-        el.classList.remove('active')
-      })
-      event.target.classList.add('active')
-
-      event.target.querySelector('.menu').classList.add('visible')
-      
-     
+      if (event.target.classList.contains('dropdown')) {
+        $dropdown.querySelectorAll('.item').forEach(el=> {
+          el.classList.remove('active')
+        })
+        event.target.classList.add('active')
+        event.target.querySelector('.menu').classList.add('visible')
+      }     
     })
 
     $dropdown.querySelectorAll('.item').forEach(item => {
       item.addEventListener('click', event => {
-        $dropdown.value = event.target.querySelector('.text').value
-      })
+        event.stopPropagation()
+        console.log(event.target.value)
+/*         $dropdown.querySelector('.default.text').value = event.target.querySelector('.text').value
+ */      })
     })
-  }) */
+  })
 
 
   $(document).ready(function () {
